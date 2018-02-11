f33e7a6c9b8b8299c5d7ab899c13e3f7a9b441c9
Prakash <Prakash@DESKTOP-7AUACEO>
2018-02-11 14:13:40 -0500

    New files are added and modified.

--- src/oopproject6/OopProject6.java
+++ src/oopproject6/OopProject6.java
@@ -3,6 +3,7 @@ package oopproject6;
 import entry.Entry;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintStream;
 import java.util.Scanner;
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -15,17 +16,17 @@ public class OopProject6 {
         Entry[] entryList = new Entry[200];
         String command = null;
         String commandCode = null;
-        
+
         stdin = new Scanner(System.in);
         entryList = readInventory("inventory.txt", entryList);
         num_entries = getEntryCount(entryList);
-        
+
         System.out.println("Codes are entered as 1 to 8 characters. ");
         System.out.println("Use \"e\" for enter, \"f\" for find, \"l\" for list, \"q\" for quit. ");
         System.out.println();
         command = getCommand();
         commandCode = command.charAt(0) + "";
-        
+
         while (!command.equalsIgnoreCase("q")) {
             switch (commandCode) {
                 case "e":
@@ -41,6 +42,18 @@ public class OopProject6 {
                         System.out.println("Entry not found");
                     }
                     break;
+                case "d":
+                    index = getIndex(entryList, command);
+                    if (index >= 0) {
+                        Entry[] temp = ArrayUtils.remove(entryList, index);
+                        for (Entry entry : entryList) {
+                            entry = null;
+                        }
+                        num_entries = num_entries - 1;
+                        storeInventory("inventory.txt", temp);
+                        entryList = readInventory("inventory.txt", entryList);
+                        break;
+                    }
                 default:
                     System.err.println("Not a valid input");
             }
@@ -63,6 +76,15 @@ public class OopProject6 {
         return entryList;
     }
 
+    public static void storeInventory(String fileName, Entry[] entryList) throws FileNotFoundException {
+        PrintStream printFormat = new PrintStream(new File(fileName));
+        for (int i = 0; i < num_entries; i++) {
+            printFormat.println(entryList[i].name + " " + entryList[i].quantity + " " + entryList[i].notes);
+        }
+
+        System.out.println("Inventory stored.");
+    }
+
     private static int getEntryCount(Entry[] entryList) {
         int count = 0;
         for (Entry entry : entryList) {
@@ -76,35 +98,38 @@ public class OopProject6 {
     }
 
     private static void getEntries(String command, Entry[] entryList) {
-        String productName = command.substring(2).trim().toLowerCase();
-        String option;
         try {
+            String productName = command.substring(2).trim().toLowerCase();
+            String option;
             for (int i = 0; i < num_entries; i++) {
                 if (entryList[i].name.equalsIgnoreCase(productName)) {
                     System.err.println("Product already exists.");
-                    System.out.println();
                     return;
                 }
             }
-            System.out.println("Enter quantity : ");
+            System.out.print("Enter quantity : ");
             String quantity = stdin.next();
-            System.out.println("Enter notes : ");
+            System.out.println();
+            System.out.print("Enter notes : ");
             String notes = stdin.next();
             entryList[num_entries] = new Entry(productName, quantity, notes);
+            System.out.println();
             num_entries++;
         } catch (ArrayIndexOutOfBoundsException ex) {
             System.err.println("Entry is full");
+            System.exit(0);
+        } catch (StringIndexOutOfBoundsException ex) {
+            System.err.println("Please enter a valid command");
         }
     }
-
     private static int getIndex(Entry[] entryList, String command) {
         String productName = command.substring(2).trim();
         if (ArrayUtils.isEmpty(entryList)) {
             return -1;
         } else {
             int index = 0;
-            for (int i = 0; i < 200; i++) {
-                if (entryList[i].name.equalsIgnoreCase(productName)) {
+            for (int i = 0; i < num_entries; i++) {
+                if (entryList[i].name.equalsIgnoreCase(productName) && entryList[i] != null) {
                     index = i;
                     break;
                 } else {
@@ -120,7 +145,6 @@ public class OopProject6 {
         System.out.print("Enter command: ");
         command = stdin.nextLine().toLowerCase();
         command = command.trim();
-        System.out.println();
         while (command.length() > 8 || command.isEmpty()) {
             System.out.println("Sorry, command must not exceed 8 characters.");
             System.out.print("Enter command :");
