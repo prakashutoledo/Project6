2a1a11d3fd687476e47d9a1ab05e9ffcdd47db30
Prakash <Prakash@DESKTOP-7AUACEO>
2018-02-11 08:58:06 -0500

    New files are added and modified.

---
+++ src/entry/Entry.java
@@ -0,0 +1,37 @@
+package entry;
+
+public class Entry {
+    String name;
+    String quantity;
+    String notes;
+    
+    public Entry(String name, String quantity, String notes) {
+        this.name = name;
+        this.quantity = quantity;
+        this.notes = notes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
--- src/oopproject6/OopProject6.java
+++ src/oopproject6/OopProject6.java
@@ -1,7 +1,11 @@
 package oopproject6;
+
+import entry.Entry;
+import org.apache.commons.lang3.ArrayUtils;
 public class OopProject6 {
+    public static Entry[] entryList = new Entry[2];
+    
     public static void main(String[] args) {
-        
     }
     
 }
